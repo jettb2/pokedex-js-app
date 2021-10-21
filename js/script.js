0f00c5080b34623de2a150940caae3ded74f2eac
jettb2 <jettblaschka@gmail.com>
2021-10-21 11:54:14 -0400

    added pokemonList Array to script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let pokemonList = [
+  {name = 'Bulbasaur', height = 0.7, type = ['Grass','Poison']},
+  {name = 'Squirtle', height = 0.5, type = ['Water']},
+  {name = 'Lapras', height = 2.5, type = ['Ice', 'Water']},
+  {name = 'Palkia', height = 4.2, type = ['Water', 'Dragon']},
+]
