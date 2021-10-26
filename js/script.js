4db28efd0a231fcf5e88cbfdb474fb1061b80d1c
jettb2 <jettblaschka@gmail.com>
2021-10-26 19:01:21 -0400

    added spacing to array in script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 let repository = [
   {
-    name: 'Bulbasaur',
+    name: ' Bulbasaur',
     height: 0.7,
     type: ['Grass','Poison']
   },
   {
-    name: 'Squirtle',
+    name: ' Squirtle',
     height: 0.5,
     type: ['Water']
   },
   {
-    name: 'Lapras',
+    name: ' Lapras',
     height: 2.5,
     type: ['Ice', 'Water']
   },
   {
-    name: 'Palkia',
+    name: ' Palkia',
     height: 4.2,
     type: ['Water', 'Dragon']
   }
 ];
 
 for (let i = 0; i < repository.length; i++) {
-    document.write(repository[i].name + ' Height:' + repository[i].height +'m');
+    document.write(repository[i].name + ' Height: ' + repository[i].height +'m');
     if (repository[i].height > 4) {
-      document.write('- Wow that\'s a big Pokemon!');
+      document.write(' - Wow that\'s a big Pokemon!');
     }
 }
