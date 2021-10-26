eb6545e8b667525fe252b91b88f85a201561a341
jettb2 <jettblaschka@gmail.com>
2021-10-26 17:37:38 -0400

    updated pokemonList to repository and spaced out array

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
-let pokemonList = [
-  {name: 'Bulbasaur', height: 0.7, type: ['Grass','Poison']},
-  {name: 'Squirtle', height: 0.5, type: ['Water']},
-  {name: 'Lapras', height: 2.5, type: ['Ice', 'Water']},
-  {name: 'Palkia', height: 4.2, type: ['Water', 'Dragon']}
-]
+let repository = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['Grass','Poison']
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    type: ['Water']
+  },
+  {
+    name: 'Lapras',
+    height: 2.5,
+    type: ['Ice', 'Water']
+  },
+  {
+    name: 'Palkia',
+    height: 4.2,
+    type: ['Water', 'Dragon']
+  }
+];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' Height:' + pokemonList[i].height +'m'); 
+for (let i = 0; i < repository.length; i++) {
+    document.write(repository[i].name + ' Height:' + repository[i].height +'m');
 }
