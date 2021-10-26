04cc2fcfd99ba2192fedfb0bf585eba17733a031
jettb2 <jettblaschka@gmail.com>
2021-10-26 17:20:55 -0400

    upated array in script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 let pokemonList = [
-  {name = 'Bulbasaur', height = 0.7, type = ['Grass','Poison']},
-  {name = 'Squirtle', height = 0.5, type = ['Water']},
-  {name = 'Lapras', height = 2.5, type = ['Ice', 'Water']},
-  {name = 'Palkia', height = 4.2, type = ['Water', 'Dragon']},
+  {name: 'Bulbasaur', height: 0.7, type: ['Grass','Poison']},
+  {name: 'Squirtle', height: 0.5, type: ['Water']},
+  {name: 'Lapras', height: 2.5, type: ['Ice', 'Water']},
+  {name: 'Palkia', height: 4.2, type: ['Water', 'Dragon']}
 ]
 
-document.write('<ul>')
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write('<li>')
-  document.write(pokemonList[i].name + );
+    document.write(pokemonList[i].name);
 }
