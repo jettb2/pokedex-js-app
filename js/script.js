ed94c5d47cbe819accf18b947abbadb2f92833b6
jettb2 <jettblaschka@gmail.com>
2021-11-07 19:51:36 -0500

    added IIFE to pokemon array

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
 //Array for Pokemon names, height and type
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    type: ['Grass','Poison']
-  },
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    type: ['Water']
-  },
-  {
-    name: 'Lapras',
-    height: 2.5,
-    type: ['Ice', 'Water']
-  },
-  {
-    name: 'Palkia',
-    height: 4.2,
-    type: ['Water', 'Dragon']
-  }
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      type: ['Grass','Poison']
+    },
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      type: ['Water']
+    },
+    {
+      name: 'Lapras',
+      height: 2.5,
+      type: ['Ice', 'Water']
+    },
+    {
+      name: 'Palkia',
+      height: 4.2,
+      type: ['Water', 'Dragon']
+    }
 ];
+
+function getAll() {
+      return pokemonList;
+      }
+
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+
+})();
 
 function printArrayDetails(list) {
   pokemonList.forEach(function(getAll) {
@@ -29,7 +40,7 @@ function printArrayDetails(list) {
 }
 
 
-printArrayDetails();
+//printArrayDetails();
 /*the printArrayDetails below can be used to either print array details again or if there is another array, print those details
 inbetween the ()*/
 //printArrayDetails();
