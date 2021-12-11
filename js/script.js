318531d58aae9586bf232332cb23f48341ba9627
jettb2 <jettblaschka@gmail.com>
2021-12-10 19:00:04 -0500

    updated search bar in javascript

--- js/script.js
+++ js/script.js
@@ -135,3 +135,16 @@ pokemonRepository.loadList().then(function() {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+document.getElementById('search-term').addEventListener('input', e => {
+  let searchTerm = e.target.value;
+  let buttons = document.querySelectorAll('.pokemon-button')
+  for(let i = 0; i < buttons.length; i++) {
+  let button = buttons[i];
+  if(button.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+    button.parentNode.style.display = 'inline';
+  } else {
+    button.parentNode.style.display = 'none';
+  }
+}
+})
